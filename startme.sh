78124de1de6415968605b447cdd6f91963d346be
Jerry Smith <jsmith@jerry-smith.net>
2019-10-11 20:56:24 -0500

    Fix issues with building Docker image

--- startme.sh
+++ startme.sh
@@ -6,13 +6,14 @@ SKIP_REFRESH=${SKIP_REFRESH:-}
 
 if [ -z "${OVERRIDE}" ]
 then
-	mkdir -p /var/www/html
-	darkhttpd /var/www/html --port 80 --daemon
-	if [ -z "${SKIP_REFRESH}" ]
-	then
-		wget -qO- https://codeload.github.com/zenhack/simp_le/tar.gz/master | tar xz -C /
-	fi
-	/simp_le-master/simp_le.py --default_root /var/www/html "$@"
+    mkdir -p /var/www/html
+    darkhttpd /var/www/html --port 80 --daemon
+    if [ -z "${SKIP_REFRESH}" ]
+    then
+        cd /simp_le-master
+        git pull
+    fi
+    /simp_le-master/simp_le.py --default_root /var/www/html "$@"
 else
-	"$@"
+    "$@"
 fi
